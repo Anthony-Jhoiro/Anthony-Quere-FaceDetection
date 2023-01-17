5ff03a6e182bcff38a99d87b48c12229457adf39
anthony <quereantho@gmail.com>
2023-01-17 09:53:01 +0100

    :tada: first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Anthony Quéré FaceDetection
+
+Github [https://github.com/Anthony-Jhoiro/Anthony-Quere-FaceDetection](https://github.com/Anthony-Jhoiro/Anthony-Quere-FaceDetection)
---
+++ src/Anthony.Quere.FaceDetection.Console/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System.Text.Json;
+using Anthony.Quere.FaceDetection;
+
+
+var imagesBytes = new List<byte[]>();
+
+foreach (var arg in args)
+{
+    imagesBytes.Add(await File.ReadAllBytesAsync(arg));
+}
+
+var detectFaceInScenesResults = new FaceDetection().DetectInScenes(imagesBytes);
+
+foreach (var detectionResult in detectFaceInScenesResults)
+{
+    System.Console.WriteLine($"Points:{JsonSerializer.Serialize(detectionResult.Points)}");
+} 
